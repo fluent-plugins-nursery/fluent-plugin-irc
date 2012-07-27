ecbd4574fb31b8c889db21066d45138105acc349
choplin <choplin.public@gmail.com>
2012-07-28 01:53:09 +0900

    fix configure test

--- test/plugin/test_out_irc.rb
+++ test/plugin/test_out_irc.rb
@@ -6,12 +6,18 @@ class IRCOutputTest < Test::Unit::TestCase
   end
 
   CONFIG = %[
+    type irc
     host localhost
     port 6667
-    #channel #fluentd
+    channel fluentd
     nick fluentd
     user fluentd
     real fluentd
+    message notice: %s [%s] %s
+    out_keys tag,time,msg
+    time_key time
+    time_format %Y/%m/%d %H:%M:%S
+    tag_key tag
   ]
 
   def create_driver(conf = CONFIG)
@@ -23,10 +29,15 @@ class IRCOutputTest < Test::Unit::TestCase
 
     assert_equal "localhost", d.instance.host
     assert_equal 6667, d.instance.port
+    assert_equal "fluentd", d.instance.channel
     assert_equal "fluentd", d.instance.nick
     assert_equal "fluentd", d.instance.user
     assert_equal "fluentd", d.instance.real
-    p d.instance
+    assert_equal "notice: %s [%s] %s", d.instance.message
+    assert_equal ["tag","time","msg"], d.instance.out_keys
+    assert_equal "time", d.instance.time_key
+    assert_equal "%Y/%m/%d %H:%M:%S", d.instance.time_format
+    assert_equal "tag", d.instance.tag_key
   end
 
   #def test_emit
